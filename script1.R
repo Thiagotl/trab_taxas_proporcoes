e7a561d354b02a714bab302688f3bbaadfcdcdfe
Thiago Tavares <thiago.tlopess@gmail.com>
2025-04-13 20:31:48 -0300

    att

--- script1.R
+++ script1.R
@@ -1,42 +1,49 @@
 # PROBABILITY DENSITY FUNCTION
 
-dUGo <- function(x, mu=.5, sigma=1.2, tau=.5)
-{
+
+dUGo<-function(x, mu=.5, sigma=1.2){
   
-  fx<- log(tau)/(1 - mu^(-sigma)) * sigma * x^(-(1 + sigma)) * exp((log(tau) / (1 - mu^(-sigma))) *
-                                                                     (1 - x^(-sigma)))
+  fx<-mu*sigma * (x^-(sigma+1)) * exp(-mu*(x^(-sigma)-1))
+  
   return(fx)
   
 }
 
-integrate(dUGo,0.01, 0.99) 
-
 # CUMULATIVE DISTRIBUTION FUNCTION 
-
-pUGo <-  function(q, mu=.5, sigma=1.2, tau=.5) 
-{
-  cdf <- exp(((log(tau)) / (1 - mu^(-sigma))) * (1 - q^(-sigma)))
+pUGo<-function(q, mu=.5, sigma=1.2){
+  
+  cdf<-exp(-mu*(q^(-sigma)-1))
+  
   return(cdf)
+  
 }
+
 
 pUGo(.25)
 integrate(dUGo, 0, .25)
 
 # QUANTILE FUNCTION
-qUGo<-function(u, mu=.5, sigma=1.2, tau=.5)
+qUGo<-function(u, mu=.5, sigma=1.2)
 {
-  q<-(1 - (log(u) * (1 - mu^(-sigma)) / log(tau)))^(-1 / sigma)
-  q
+  q<-((-log(u)/mu)+1)^(-1/sigma)
+  
+  return(q)
 }
 
 u=pUGo(.82)
-qUGo(u,mu=.5, sigma=1.2)
+u
+
+qUGo(u)
+
 
 
 # INVERSION METHOD FOR RANDOM GENERATION
 
 rUGO <- function(n, mu=.5, sigma=1.2, tau=.5) {
   u <- runif(n)
-  y <- (1 - (log(u) * (1 - mu^(-sigma)) / log(tau)))^(-1 / sigma)
+ 
   return(y)
 }
+
+
+
